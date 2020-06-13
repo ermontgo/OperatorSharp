7f80174ab44781b9e841c54fd8a5d590bd033c9b
Eric Montgomery <emontgomery@10-4.com>
2020-06-12 23:25:05 -0600

    Added operator extensions

---
+++ src/OperatorSharp.Extensions.DependencyInjection/OperatorDependencyInjectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OperatorSharp;
+using OperatorSharp.CustomResources;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class OperatorDependencyInjectionExtensions
+    {
+        public static ServiceCollection AddOperator<TResource, TOperator>(this ServiceCollection services) where TResource : CustomResource
+            where TOperator : Operator<TResource>
+        {
+            services.AddScoped<IEventRecorder<TResource>, EventRecorder<TResource>>();
+            services.AddScoped<TOperator>();
+
+            return services;
+        }
+    }
+}
