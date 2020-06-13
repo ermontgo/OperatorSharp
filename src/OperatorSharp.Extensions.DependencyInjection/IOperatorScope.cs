d366b5786d2d31878f66d568ed5aad36ed34558b
Eric Montgomery <emontgomery@10-4.com>
2020-06-12 22:17:15 -0600

    Added OperatorScope

---
+++ src/OperatorSharp.Extensions.DependencyInjection/IOperatorScope.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OperatorSharp.Extensions.DependencyInjection
+{
+    public interface IOperatorScope : IDisposable
+    {
+        Task StartAsync(string watchedNamespace, CancellationToken token);
+    }
+}
---
+++ src/OperatorSharp.Extensions.DependencyInjection/OperatorScope.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OperatorSharp.Extensions.DependencyInjection
+{
+    public class OperatorScope<TOperator> : IOperatorScope where TOperator : OperatorSharp.Operator
+    {
+        private readonly IServiceScope scope;
+        public OperatorScope(IServiceProvider provider)
+        {
+            this.scope = provider.CreateScope();
+        }
+
+        public async Task StartAsync(string watchedNamespace, CancellationToken token)
+        {
+            var instance = scope.ServiceProvider.GetService<TOperator>();
+            var logger = scope.ServiceProvider.GetService<ILogger<OperatorScope<TOperator>>>();
+
+            try
+            {
+                await instance.WatchAsync(token, watchedNamespace);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("An error occurred during operator execution", ex);
+            }
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    scope.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+        }
+        #endregion
+
+    }
+}
